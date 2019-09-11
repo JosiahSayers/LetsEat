6d035899b19e37127a7ff374a41f4bef3f9df3e1
Josiah Sayers <josiah.sayers15@gmail.com>
2019-09-10 21:31:08 -0400

    Adds an empty project using the angular dotnetcore template

---
+++ LetsEat_AngularTemplate/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LetsEat_AngularTemplate.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsEat_AngularTemplate.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
